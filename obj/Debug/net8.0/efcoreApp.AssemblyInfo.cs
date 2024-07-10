43facb315bcc80afea02ce8d56dd93971787b26f
Kurban KOTAN <kurbankotan@gmail.com>
2024-07-10 10:56:21 +0300

    Entity Framwork Migrations Update for Initialization

--- obj/Debug/net8.0/efcoreApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/efcoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("efcoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46cce8f894b6229600eb017a03edccc6e341e236")]
 [assembly: System.Reflection.AssemblyProductAttribute("efcoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("efcoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
