3e33378698e4fab927c232a1c0e9bdee83549236
Kurban KOTAN <kurbankotan@gmail.com>
2024-07-18 10:54:25 +0300

    Course Edit

--- Controllers/KursController.cs
+++ Controllers/KursController.cs
@@ -35,5 +35,65 @@ namespace efcoreApp.Controllers
         }
 
 
+
+       //Kurs güncelleme
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+
+            var kurs = await _context.Kurslar.FindAsync(id); //FinAsync ile sadece Id ile arama yapılır
+            // var orgr = await _context.Kurslar.FirstOrDefaultAsync(o => o.Tarihler == Tarihler ) // Sadece Id ile değil diğer alanlarla da arama yapılıp getirilir
+           
+            if(kurs==null)
+            {
+                return NotFound();
+            }
+
+            return View(kurs);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] // Sayfada form içinde hiddden olarak oluşturulan __RequestVerificationToken'nin oluşturulması istenilir.
+                                   // Böylelikle formu gönderenin isteyen kişi ile aynı olduğu denetlenmiş olur
+        public async Task<IActionResult> Edit(int id, Kurs model)
+        {
+            if(id != model.KursId)
+            {
+                return NotFound();
+            }
+
+
+           
+            if(ModelState.IsValid)
+            {
+
+                try
+                {
+                    _context.Update(model);
+                    await _context.SaveChangesAsync();
+                }
+                catch(DbUpdateException) // Daha genel Db hataları için
+                {
+                    if(!_context.Kurslar.Any(o => o.KursId == model.KursId)) // Düzeltilmek istenilen kayıt veritabanında yoksa
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+
     }
 }
